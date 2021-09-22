b7ba0a922f0e31578c4eb45e76e8a23f5440833e
wwowann <ivasin75@mail.ru>
2021-09-22 14:18:26 +0300

    version first

--- src/main/java/com/myLesson/demo/DemoApplication.java
+++ src/main/java/com/myLesson/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 		System.out.println(context.getBean("devProfile"));
-		System.out.println(context.getBean("prodProfile"));
+		System.out.println(context.getBean("productionProfile"));
 	}
 
 }
